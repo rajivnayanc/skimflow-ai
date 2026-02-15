444260bedfa6c4b9f017a4dfd1d69525b579ae3d
Rajiv Nayan Choubey <rajivnayanc@gmail.com>
2026-02-15 17:54:51 +0530

    [ADD] Zip support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './public/manifest.json'
+import { resolve } from 'path'
+import { rmSync } from 'fs'
 
 export default defineConfig({
     build: {
-        minify: true, // or 'terser' or 'esbuild'
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            },
+            format: {
+                comments: false,
+            },
+        },
+        sourcemap: false,
     },
-    plugins: [crx({ manifest })],
+    plugins: [
+        crx({ manifest }),
+        {
+            name: 'clean-vite-manifest',
+            closeBundle() {
+                try { rmSync(resolve('dist', '.vite'), { recursive: true, force: true }) } catch (e) { }
+            }
+        }
+    ],
 })
